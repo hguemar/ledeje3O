ce694677ba9c462b0741f40ae95b32a484b6eb90
Alexandre LAURANS <ancitik@protonmail.com>
2019-05-16 15:01:20 +0200

    Add article create form

--- routes/posts.js
+++ routes/posts.js
@@ -1,13 +1,15 @@
 module.exports = function(app) {
   app.get("/post/create", function(req, res) {
-    res.send("XXX");
+    //Render form
+    res.render("createArticle");
   });
 
   app.post("/post/create", function(req, res) {
-    res.send("XXX");
+    //Execute CRUD operation to Create article in MongoDB
+    res.send("Article created");
   });
 
   app.get("/post/:id", function(req, res) {
     res.send("XXX");
   });
-}
+};
