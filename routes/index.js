d729394118b0aac2cb7a0ddcab5fb0904017aa70
HAENSLER <julien.haensler@etu.univ-lyon1.fr>
2019-05-16 17:16:19 +0200

    pagination OK

--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,23 @@ var fixtures = require("./fixtures");
 
 module.exports = function(app) {
 
+
+  app.get("/", function(req, res){
+    res.redirect("/1");
+  });
+
   app.get("/:page", function(req, res) {
 
       var page = req.params.page;
       app.db.collection("articles").find().sort({date: -1}).toArray().then(function(result) {
 
-          var length = result.length;
-          result.forEach(function(element) {
+          var length = result.length / 5;
+          // je tronque mon tableau pour envoyer à la vue uniquement les articles à afficher selon la page
+          var toView = result.slice(((page*5)-5),(page*5));
+          toView.forEach(function(element) {
               element.content = summarize(element.content.toString());
           });
-          res.render("index", {'result': result, 'length': length});
+          res.render("index", {'result': toView, 'length': length});
 
       });
   });
