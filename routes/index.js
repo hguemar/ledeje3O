85fa75a5c6febb9987e678a50b60ac97a6f648e7
nosql <nosql@localhost.localdomain>
2019-05-16 20:28:56 +0200

    First stage

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var posts = require("./posts");
 module.exports = function(app) {
 
   app.get("/", function(req, res) {
-    res.render("index");
+    app.db.collection('article').find({}).toArray(function(err, doc) {
+      res.render("index", {'data': doc});
+    });
   });
 
   // Register posts endpoint
--- routes/posts.js
+++ routes/posts.js
@@ -1,13 +1,21 @@
-module.exports = function(app) {
-  app.get("/post/create", function(req, res) {
-    res.send("XXX");
+var ArticleService = require("../services/ArticleService");
+
+module.exports = function (app) {
+  app.get("/post/create", function (req, res) {
+    res.render("create");
   });
 
-  app.post("/post/create", function(req, res) {
-    res.send("XXX");
+  app.post("/post/create", function (req, res) {
+    let articleService = new ArticleService();
+    articleService.registerArticle(app, req)
+      .then(result => res.send("L'article est bien ajouté."))
+      .catch(err => { throw Error(err) });
   });
 
-  app.get("/post/:id", function(req, res) {
-    res.send("XXX");
+  app.get("/post/:id", function (req, res) {
+    let articleService = new ArticleService();
+    articleService.deleteArticle(app, req)
+      .then(result => res.send("L'article est bien supprimé."))
+      .catch(err => { throw Error(err) });
   });
 }
