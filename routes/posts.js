5d763877e8c1fe3bdb4c74161a89ed58a6f26336
HAENSLER <julien.haensler@etu.univ-lyon1.fr>
2019-05-16 11:11:52 +0200

    Affichage d'un article

--- routes/posts.js
+++ routes/posts.js
@@ -1,3 +1,5 @@
+ObjectID = require('mongodb').ObjectID;
+
 module.exports = function(app) {
   app.get("/post/create", function(req, res) {
     //Render form
@@ -9,7 +11,19 @@ module.exports = function(app) {
     res.send("Article created");
   });
 
+  // Fonction pour afficher un seul article
   app.get("/post/:id", function(req, res) {
-    res.send("XXX");
+
+    //Récupération du paramètre id en get
+    var id = req.params.id;
+
+    // requête sur la bdd avec comme paramètre l'id de l'article
+    app.db.collection("articles").find({"_id": new ObjectID(id)}).toArray().then(function(result) {
+
+      // On rend sur la vue article avec le contenu, etc ...
+      res.render("article", {'title': result[0].title, 'content': result[0].content, 'author': result[0].author, 'date': result[0].date});
+
+    });
+
   });
 };
