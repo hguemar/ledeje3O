cad2575a3afe0b95b8f4b46d12356da42ad5073d
HAENSLER <julien.haensler@etu.univ-lyon1.fr>
2019-05-16 16:23:31 +0200

    added home page and article page

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,15 @@ var fixtures = require("./fixtures");
 
 module.exports = function(app) {
 
-  app.get("/", function(req, res) {
+  app.get("/:page", function(req, res) {
 
-      app.db.collection("articles").find().toArray().then(function(result) {
+      var page = req.params.page;
+      app.db.collection("articles").find().sort({date: -1}).toArray().then(function(result) {
 
-          var length = result.length();
+          var length = result.length;
+          result.forEach(function(element) {
+              element.content = summarize(element.content.toString());
+          });
           res.render("index", {'result': result, 'length': length});
 
       });
@@ -17,9 +21,7 @@ module.exports = function(app) {
   posts(app);
   fixtures(app);
 
-  function resume(article){
-    article.substring(0, 140);
-    article += " ...";
-    return article;
+  function summarize(article){
+    return article.substring(0,140) + " ...";
   }
-}
+};
