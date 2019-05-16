f15959e5d37ce9324093837b80e14156c7058a82
nosql <nosql@localhost.localdomain>
2019-05-16 22:07:34 +0200

    Seconds status

--- routes/posts.js
+++ routes/posts.js
@@ -1,4 +1,5 @@
 var ArticleService = require("../services/ArticleService");
+var ObjectID = require('mongodb').ObjectID;
 
 module.exports = function (app) {
   app.get("/post/create", function (req, res) {
@@ -13,9 +14,16 @@ module.exports = function (app) {
   });
 
   app.get("/post/:id", function (req, res) {
+    let id = req.params.id;
+    app.db.collection('article').find({ "_id": new ObjectID(id) }).toArray(function (err, doc) {
+      res.render("article", { 'doc': doc[0] });
+    });
+  });
+
+  app.get("/post/delete/:id", function (req, res) {
     let articleService = new ArticleService();
     articleService.deleteArticle(app, req)
-      .then(result => res.send("L'article est bien supprimé."))
-      .catch(err => { throw Error(err) });
+      .then(result => res.redirect('/'))
+      .catch(err => { throw Error(err) })
   });
 }
