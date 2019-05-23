2e530fdf147f0b58aa15143472a1c833a354664e
nosql <nosql@localhost.localdomain>
2019-05-23 18:27:59 +0200

    Creation routes blog

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,33 @@
 var posts = require("./posts");
+var faker = require('faker');
+var uuid = require('uuid/v1');
 
 module.exports = function(app) {
 
   app.get("/", function(req, res) {
-    res.render("index");
+	app.db.collection('articles').find({}).toArray(function (err, doc) {
+		 doc.forEach(element => {
+       			 element.contenu = summarize(element.contenu);
+      });
+      res.render("index", { 'data': doc });
+    });
   });
 
+  app.get("/generate", function(req, res) {
+	app.db.collection("articles").remove();
+	for(let i = 0; i < 10; i++){
+		let auteur = faker.name.findName();
+		let titre = faker.lorem.sentence();
+		let contenu = faker.lorem.paragraphs(5);
+		let article = {id: uuid(), auteur:auteur, titre:titre, contenu:contenu, date:new Date()};
+		app.db.collection("articles").insertOne(article);
+	}
+	res.redirect("/");		
+  });
   // Register posts endpoint
   posts(app);
+}
+
+function summarize(value){
+	return value.substr(0,140) + "...";
 }
--- routes/posts.js
+++ routes/posts.js
@@ -1,13 +1,30 @@
+var uuid = require('uuid/v1');
+
 module.exports = function(app) {
   app.get("/post/create", function(req, res) {
-    res.send("XXX");
+    res.render("create");
   });
 
   app.post("/post/create", function(req, res) {
-    res.send("XXX");
+	let titre = req.body.titre;
+	let contenu = req.body.contenu;
+	let auteur = req.body.auteur;
+	if(typeof(titre) != 'undefined' && typeof(contenu) != 'undefined' && typeof(auteur) != 'undefined'){
+		let article = {titre:titre, contenu:contenu, auteur:auteur, date:new Date(), id:uuid()};
+		app.db.collection("articles").insertOne(article);
+		res.redirect("/");
+	}
+	return Promise.reject();
   });
 
   app.get("/post/:id", function(req, res) {
-    res.send("XXX");
+	app.db.collection("articles").find({"id": req.params.id}).toArray(function(err, article){
+		res.render("article", {'article': article[0]});
+	});
+  });
+
+  app.get("/post/delete/:id", function(req, res) {
+	app.db.collection("articles").deleteOne({"id": req.params.id});
+	res.redirect("/");
   });
 }
